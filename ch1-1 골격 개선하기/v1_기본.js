9dba7836c4559be875b4d98f83c9d01d9a4c5f12
hagyuho <hagyuho90@gmail.com>
2021-09-22 23:19:04 +0900

    2

--- ch1-1 골격 개선하기/v1_기본.js
+++ ch1-1 골격 개선하기/v1_기본.js
@@ -1,19 +1,20 @@
 import invoices from "../invoices.js";
 import plays from "../plays.js";
 
-
-
 function statement(invoice, plays) {
+  // [변수] 지역변수 선언
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
-
+  
+  // [함수] 달러 포메팅 함수 선언
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
   }).format;
 
+  // [로직] for 반복문으로 총액 계산 로직
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = 0;
@@ -38,12 +39,12 @@ function statement(invoice, plays) {
         throw new Error(`알 수 없는 장르 : ${play.type}`);
     }
 
-    //포인트를 적립한다.
+    // [로직] 포인트를 적립 계산
     volumeCredits += Math.max(perf.audience - 30, 0);
     //희극 관객 5명마다 추가 포인트를 제공한다.
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
 
-    //청구내역을 출력한다.
+    // [출력] 청구내역 출력.
     result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience
       }석)\n`;
     totalAmount += thisAmount;
